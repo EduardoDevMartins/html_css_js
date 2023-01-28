90f989d1e78e022736ab2ba034a9b4226606664f
EduardoDevMartins <eduardodevmartins@gmail.com>
2023-01-27 22:38:20 -0300

    primeiros passos

---
+++ HTML&CSS/primeirosite/Logica de programacao/operadores.js
@@ -0,0 +1,9 @@
+const sum = 2 + 4;
+const diff = 10 - 5;
+const multiply = 20 * 556;
+const division = 10 / 2;
+const module = 24 % 5;
+const power = Math.pow(5, 2);
+const squareRoot = Math.sqrt(9);
+
+console.log(sum, diff, multiply, division, module, power, squareRoot);
---
+++ HTML&CSS/primeirosite/Logica de programacao/variables.js
@@ -0,0 +1,7 @@
+const name = "Eduardo"
+const age = "31"
+const teacher = true;
+
+console.log(`Oi, o meu nome é ${name} e eu tenho ${age} anos.`);
+
+
